52f0242e68135a3b85a7c7e1f13530a19a1548dc
Peter Hultqvist <hultqvist@silentorbit.com>
2025-12-30 22:44:32 +0100

    NuGet publish all at once locally

---
+++ JsonDiff/Publish All NuGet.bat
@@ -0,0 +1,4 @@
+dotnet pack JsonDiff.Abstractions\JsonDiff.Abstractions.csproj -o C:\LocalNuGet
+dotnet pack JsonDiff.Generator\JsonDiff.Generator.csproj -o C:\LocalNuGet
+dotnet pack JsonDiff.NuGet\JsonDiff.NuGet.csproj -o C:\LocalNuGet
+pause
